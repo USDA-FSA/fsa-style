057c2e6335ca4df2c1a1194cc9084a3839ae51df
Francis Rupert <francisrupert@gmail.com>
2016-03-16 13:58:54 -0500

    grunt copy uswds assets

--- Gruntfile.js
+++ Gruntfile.js
@@ -6,14 +6,6 @@ module.exports = function (grunt) {
     sass: {
       dev: {
         files: {
-          'public/stylesheets/main.css': 'public/sass/main.scss',
-          'public/stylesheets/main-ie6.css': 'public/sass/main-ie6.scss',
-          'public/stylesheets/main-ie7.css': 'public/sass/main-ie7.scss',
-          'public/stylesheets/main-ie8.css': 'public/sass/main-ie8.scss',
-          'public/stylesheets/elements-page.css': 'public/sass/elements-page.scss',
-          'public/stylesheets/elements-page-ie6.css': 'public/sass/elements-page-ie6.scss',
-          'public/stylesheets/elements-page-ie7.css': 'public/sass/elements-page-ie7.scss',
-          'public/stylesheets/elements-page-ie8.css': 'public/sass/elements-page-ie8.scss',
           'public/stylesheets/prism.css': 'public/sass/prism.scss',
         },
         options: {
@@ -27,44 +19,11 @@ module.exports = function (grunt) {
     // Copies templates and assets from external modules and dirs
     copy: {
 
-      govuk_template: {
-        src: 'node_modules/govuk_template_mustache/views/layouts/govuk_template.html',
-        dest: 'govuk_modules/views/',
-        expand: true,
-        flatten: true,
-        filter: 'isFile'
-      },
-
-      govuk_assets: {
-        files: [
-          {
-            expand: true,
-            src: '**',
-            cwd: 'node_modules/govuk_template_mustache/assets',
-            dest: 'govuk_modules/public/'
-          }
-        ]
-      },
-
-      govuk_frontend_toolkit_scss: {
+      uswds_assets: {
         expand: true,
         src: '**',
-        cwd: 'node_modules/govuk_frontend_toolkit/stylesheets/',
-        dest: 'govuk_modules/public/sass/'
-      },
-
-      govuk_frontend_toolkit_js: {
-        expand: true,
-        src: '**',
-        cwd: 'node_modules/govuk_frontend_toolkit/javascripts/',
-        dest: 'govuk_modules/public/javascripts/'
-      },
-
-      govuk_frontend_toolkit_img: {
-        expand: true,
-        src: '**',
-        cwd: 'node_modules/govuk_frontend_toolkit/images/',
-        dest: 'govuk_modules/public/images/icons/'
+        cwd: 'node_modules/uswds/src',
+        dest: 'src/lib/uswds'
       },
 
     },
@@ -105,10 +64,7 @@ module.exports = function (grunt) {
   });
 
   grunt.registerTask('default', [
-    'copy:govuk_assets',
-    'copy:govuk_frontend_toolkit_scss',
-    'copy:govuk_frontend_toolkit_js',
-    'copy:govuk_frontend_toolkit_img',
+    'copy:uswds_assets',
     'sass',
   ]);
 
