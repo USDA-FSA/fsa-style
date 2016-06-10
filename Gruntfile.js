9f0396961c0a5181f9317fcb0428594d48c1494d
Francis Rupert <francisrupert@gmail.com>
2016-06-10 10:19:30 -0500

    Build Control Grunt task cleaned up [ #15 ]

--- Gruntfile.js
+++ Gruntfile.js
@@ -194,13 +194,6 @@ module.exports = function (grunt) {
           tag: '<%= pkg.version %>'
         }
       },
-      // heroku: {
-      //   options: {
-      //     remote: 'git@heroku.com:example-heroku-webapp-1988.git',
-      //     branch: 'master',
-      //     tag: pkg.version
-      //   }
-      // },
       local: {
         options: {
           remote: '../',
