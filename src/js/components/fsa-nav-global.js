75200926c2c15508b66f6dc6fa5b2d8d0b565003
Francis C. Rupert <francisrupert@gmail.com>
2017-09-01 09:01:43 -0500

    forEach menuItem ftw [amends #182]

--- src/js/components/fsa-nav-global.js
+++ src/js/components/fsa-nav-global.js
@@ -1,35 +1,41 @@
-var menuItem = document.querySelector('.fsa-nav-global__link--has-sub-menu');
+var menuItem = document.querySelectorAll('.fsa-nav-global__link--has-sub-menu');
 
-menuItem.onclick = function(e) {
+menuItem.forEach(function(item) {
 
-  var $self = this;
-  var $component = $self.closest('.fsa-nav-global');
-  var $listItem = $self.closest('.fsa-nav-global__list-item');
-  var $target = $listItem.querySelector('.fsa-nav-global__sub-menu');
+  item.classList.add('OUTLINE');
+  item.onclick = function(e) {
 
-  $target.classList.add('OUTLINE');
+    var $self = this;
+    var $component = $self.closest('.fsa-nav-global');
+    var $listItem = $self.closest('.fsa-nav-global__list-item');
+    var $target = $listItem.querySelector('.fsa-nav-global__sub-menu');
 
-  var menuState = $self.getAttribute('aria-expanded');
+    $target.classList.add('OUTLINE');
 
-  // TOGGLE MENU ITEM OPENED STATE
-  if (menuState == 'true') {
-    // $self.classList.remove('OUTLINE');
-    $self.setAttribute('aria-expanded', 'false');
-    $target.setAttribute('aria-hidden', 'true');
-  } else {
-    // $self.classList.add('OUTLINE');
-    $self.setAttribute('aria-expanded', 'true');
-    $target.setAttribute('aria-hidden', 'false');
+    var menuState = $self.getAttribute('aria-expanded');
+
+    // TOGGLE MENU ITEM OPENED STATE
+    if (menuState == 'true') {
+      // $self.classList.remove('OUTLINE');
+      $self.setAttribute('aria-expanded', 'false');
+      $target.setAttribute('aria-hidden', 'true');
+    } else {
+      // $self.classList.add('OUTLINE');
+      $self.setAttribute('aria-expanded', 'true');
+      $target.setAttribute('aria-hidden', 'false');
+    }
+
+    // if ($self.classList.contains('OUTLINE')) {
+    //   $self.classList.remove('OUTLINE');
+    //   $target.setAttribute('aria-hidden', 'true');
+    // } else {
+    //   $self.classList.add('OUTLINE');
+    //   $target.setAttribute('aria-hidden', 'false');
+    // }
+    //
   }
 
-  // if ($self.classList.contains('OUTLINE')) {
-  //   $self.classList.remove('OUTLINE');
-  //   $target.setAttribute('aria-hidden', 'true');
-  // } else {
-  //   $self.classList.add('OUTLINE');
-  //   $target.setAttribute('aria-hidden', 'false');
-  // }
-  //
-}
+});
+
 
 module.exports = FSA_NAV_GLOBAL;
