a8b94aec5cb2e18cc142ef61fdbdaba00858cefb
Casey Hill <bignamehere@gmail.com>
2018-06-19 09:08:45 -0500

    adding JS files to push branch [appends #237]

--- src/js/components/fsa-stepped-control.js
+++ src/js/components/fsa-stepped-control.js
@@ -2,68 +2,70 @@
 // None of this is production-quality. Do not use for production. Use as inspiration and guidance for yours.
 // None of this is production-quality. Do not use for production. Use as inspiration and guidance for yours.
 // None of this is production-quality. Do not use for production. Use as inspiration and guidance for yours.
-// None of this is production-quality. Do not use for production. Use as inspiration and guidance for yours.
-// None of this is production-quality. Do not use for production. Use as inspiration and guidance for yours.
-// None of this is production-quality. Do not use for production. Use as inspiration and guidance for yours.
-// None of this is production-quality. Do not use for production. Use as inspiration and guidance for yours.
-// None of this is production-quality. Do not use for production. Use as inspiration and guidance for yours.
-// None of this is production-quality. Do not use for production. Use as inspiration and guidance for yours.
 
-var $ = window.jQuery = require('jquery');
 
-;$(function() {
+// UTILITY METHODS
+var getOffsetTop = function (elem) {
 
-    var $steppedControl = $('.fsa-stepped-control--sticky'); // only the --sticky ones
+	// Set our distance placeholder
+	var distance = 0;
 
-    function steppedControl() {
+	// Loop up the DOM
+	if (elem.offsetParent) {
+		do {
+			distance += elem.offsetTop;
+			elem = elem.offsetParent;
+		} while (elem);
+	}
 
-      $steppedControl.each(function(index) {
+	// Return our distance
+	return distance < 0 ? 0 : distance;
+};
 
-        var $self = $(this)
-        var pageTop = $(window).scrollTop();
-        var windowHeight = $(window).height();
-        var steppendControlPosTop = $self.offset().top - pageTop;
-        var steppendControlHeight = $self.outerHeight();
-        var steppendControlPosBot = windowHeight - (steppendControlPosTop + steppendControlHeight);
+//
+//
+var stickySteps = document.querySelectorAll('.fsa-stepped-control--sticky');
 
-        // console.log('pageTop: ' + pageTop);
-        // console.log('windowHeight: ' + windowHeight);
-        // console.log('steppendControlPosTop: ' + steppendControlPosTop);
-        // console.log('steppendControlHeight: ' + steppendControlHeight);
-        // console.log('steppendControlPosBot: ' + steppendControlPosBot);
+function setComponentStyle() {
 
-        if (steppendControlPosBot > 12) {
-          $self.addClass("fsa-stepped-control--unstuck");
-        }
-        else {
-          $self.removeClass("fsa-stepped-control--unstuck");
-        }
+  // iterate thru each stepped control on page
+  forEach(stickySteps, function(index, value) {
+    var _el = value;
 
-      });
+    var scBottomPosition = window.innerHeight - (_el.getOffsetTop() + _el.offsetHeight)
 
+    if (scBottomPosition > 12) {
+      _el.addClass("fsa-stepped-control--unstuck");
+    } else {
+      _el.removeClass("fsa-stepped-control--unstuck");
     }
 
-    if ($steppedControl.length) { // only run if at least one instance
+  });
 
-      $(window).scroll(function() {
-        steppedControl()
-      });
+}
 
-      $('.fsa-modal').scroll(function() {
-        steppedControl()
-      });
 
-      $(document).ready(function() {
-        steppedControl();
-      })
+// check if SC component exists on page
+if(stickySteps.length){
 
-      $(window).resize(function() {
-        // may want to **debounce** this, e.g. http://benalman.com/projects/jquery-throttle-debounce-plugin/
-        steppedControl();
-      })
+  window.addEventListener('scroll', function() {
+    setComponentStyle();
+  });
 
-    }
+  var modal = document.querySelector('.fsa-modal');
+  modal.addEventListener("scroll", function(){
+    setComponentStyle();
+  });
 
-});
+  document.addEventListener("DOMContentLoaded", function(){
+    setComponentStyle();
+  });
+
+  window.addEventListener('resize', function() {
+    setComponentStyle();
+  });
+
+}
+
 
 console.log('SteppedControl loaded, its JS is NOT to be used for Production, demo purposes only');
