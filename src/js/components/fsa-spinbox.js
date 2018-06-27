17305c56f46579222b3d58a2f37d664a4e903de6
Francis Rupert <francisrupert@gmail.com>
2018-06-27 13:58:12 -0500

    Refined Spinbox's Vanilla JS (including brute-force IE backfall) [#180]

--- src/js/components/fsa-spinbox.js
+++ src/js/components/fsa-spinbox.js
@@ -9,6 +9,10 @@
 // None of this is production-quality. Do not use for production. Use as inspiration and guidance for yours.
 // None of this is production-quality. Do not use for production. Use as inspiration and guidance for yours.
 
+var ieIE_10_down = (function() { if (new RegExp("MSIE ([0-9]{1,}[\.0-9]{0,})").exec(navigator.userAgent) != null) { return parseFloat( RegExp.$1 ); } else { return false; } })();
+var isIE_11 = '-ms-scroll-limit' in document.documentElement.style && '-ms-ime-align' in document.documentElement.style;
+
+// What gets clicked?
 var spinbox__triggers = document.querySelectorAll('[data-behavior~="spinbox-spin"]');
 
 // Utility method to loop thru NodeList correctly
@@ -50,7 +54,7 @@ var spinbox__hasClass = function(el, classname) {
   return (' ' + el.className + ' ').indexOf(' ' + classname + ' ') > -1;
 }
 
-// iterate thru trigger elements and set click handler
+// Iterate thru trigger elements and set click handler
 spinbox__forEach(spinbox__triggers, function (index, value) {
 
   var _el = value;
@@ -58,15 +62,30 @@ spinbox__forEach(spinbox__triggers, function (index, value) {
   _el.addEventListener('click', function(e){
 
     var _component = spinbox__getClosest(e.currentTarget, '.fsa-spinbox');
-    var _target = _component.querySelector('.fsa-spinbox__input');
+    var _target = _component.querySelector('.fsa-spinbox__input'); // Which form element in this component will be updated with new value?
+    var _targetStepAmount = _target.getAttribute('step'); // By how much will we increment/decrement?
 
-    if (spinbox__hasClass(_el, 'fsa-spinbox__btn--decrement')){
-      _target.stepDown();
-      console.log('Spinbox decremented');
+    if (_targetStepAmount == null) {
+      // If step attribute doesn't exist, the default value is "1"
+      _targetStepAmount = 1;
+    }
+
+    // For Demo purposes, we're only demo'ing in browsers that *natively* support stepUp() and stepDown()
+    if (ieIE_10_down || isIE_11) { // lazily targeting IE 11 and below. For all I know it works above IE 11
+      // Just alert() some basic helpfup info
+      alert('Step up/down by ' + _targetStepAmount);
     }
     else {
-      _target.stepUp();
-      console.log('Spinbox incremented');
+      if (spinbox__hasClass(_el, 'fsa-spinbox__btn--decrement')){
+        // If you click the "Down" button SUBTRACT the step value from the current value
+        _target.stepDown();
+        console.log('Spinbox decremented');
+      }
+      else {
+        // If you click the "Up" button ADD the step value from the current value
+        _target.stepUp();
+        console.log('Spinbox incremented');
+      }
     }
 
   }, false);
