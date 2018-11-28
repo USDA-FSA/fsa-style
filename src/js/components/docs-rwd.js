a2fb41a413f4fbc3063913d121e711208866c4e2
Francis Rupert <francisrupert@gmail.com>
2018-11-28 08:56:00 -0600

    Stablized RWD Viewer [#272] and applied to all CSS Util

--- src/js/components/docs-rwd.js
+++ src/js/components/docs-rwd.js
@@ -1,6 +1,6 @@
 var $ = window.jQuery = require('jquery');
 
-$('body').on('click', '[data-behavior~="toggle-codepen-size"]', function(event) {
+$('body').on('click', '[data-behavior~="toggle-rwd-size"]', function(event) {
 
   var $self = $(this);
   var $target = $('#' + $self.attr('data-target'));
@@ -8,8 +8,8 @@ $('body').on('click', '[data-behavior~="toggle-codepen-size"]', function(event) 
   var $component = $self.closest('[data-component]');
 
   $target
-    .removeClass('docs__codepen-embed--phone docs__codepen-embed--phone-big docs__codepen-embed--tablet docs__codepen-embed--desktop')
-    .addClass('docs__codepen-embed--' + $newClass)
+    .removeClass('docs__rwd-embed--phone docs__rwd-embed--phone-big docs__rwd-embed--tablet docs__rwd-embed--desktop')
+    .addClass('docs__rwd-embed--' + $newClass)
   ;
 
   $self
@@ -23,4 +23,4 @@ $('body').on('click', '[data-behavior~="toggle-codepen-size"]', function(event) 
 })
 
 
-console.log('DocsCodepen loaded, its JS is NOT to be used for Production, demo purposes only');1
+console.log('DocsRWD loaded, its JS is NOT to be used for Production, demo purposes only');1
--- src/js/main.js
+++ src/js/main.js
@@ -13,7 +13,7 @@
 
 var DocsNav = require('./components/docs-nav');
 var DocsColor = require('./components/docs-color');
-var DocsCodepen = require('./components/docs-codepen');
+var DocsRWD = require('./components/docs-rwd');
 var NavGlobalComponent = require('./components/fsa-nav-global');
 var ModalComponent = require('./components/fsa-modal');
 var GrowlComponent = require('./components/fsa-growl');
